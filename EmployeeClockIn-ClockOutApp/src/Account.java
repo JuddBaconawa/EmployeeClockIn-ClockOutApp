cf3d90a1369dd431ef9a18078c027cb1b823e4a0
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-12 17:29:39 -0400

    GUI.java and Account.java modified
    Account constructor created for when accounts are created.
    getUsername method created.
    Account attributes added.
    
    GUI constructor created for the GUI to be created on, JFrame added to GUI + imports for swing + new GUI activated within the main method

--- EmployeeClockIn-ClockOutApp/src/Account.java
+++ EmployeeClockIn-ClockOutApp/src/Account.java
@@ -8,6 +8,22 @@
 
 //Account class - will contain the users to test the login and its functions
 public class Account {
+	
+
+	public Account() {
+
+		//Account's attributes
+		String username;
+		char[] password;
+		int userID;
+
+	}
+
+
+
+	public String getUsername() {
+		return ;
+	}
 
 	
 	
--- EmployeeClockIn-ClockOutApp/src/GUI.java
+++ EmployeeClockIn-ClockOutApp/src/GUI.java
@@ -1,17 +1,30 @@
+
+
 //packages
 
 
 //imports
+import javax.swing.JFrame;
 
 
 
 //main class - App
 public class GUI {
 
+
+    public GUI() {
+
+        //framing for the GUI
+        JFrame frame = new JFrame();
+        
+
+    }
+
     //main method
     public static void main(String[] args) throws Exception {
 
-
+        //activating or calling upon the method GUI
+        new GUI();
         
 
 
