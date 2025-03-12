4aaa1b3554aa47b138c9f1c367722db803e9ad8c
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-11 21:27:20 -0400

    EmployeeClockIn_ClockOutApp - Java project created
    project created - coding part via visual Studio code. Main class formatted manually.

---
+++ EmployeeClockIn-ClockOutApp/src/App.java
@@ -0,0 +1,17 @@
+//packages
+
+
+//imports
+
+
+
+//main class - App
+public class App {
+
+    //main method
+    public static void main(String[] args) throws Exception {
+
+
+        System.out.println("Hello, World!");
+    }
+}
