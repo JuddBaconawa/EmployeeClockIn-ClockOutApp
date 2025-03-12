dc8eb97a18045749c4c83011ffa329ced029d8d9
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-12 01:10:39 -0400

    Account and UserServices class createdq
    new classes created:
    
    Account  class to add accounts + password to check for credentials
    
    UserServices class to hide how things works in the backend.

---
+++ EmployeeClockIn-ClockOutApp/src/Account.java
@@ -0,0 +1,14 @@
+//packages
+
+
+
+//imports
+
+
+
+//Account class - will contain the users to test the login and its functions
+public class Account {
+
+	
+	
+}
---
+++ EmployeeClockIn-ClockOutApp/src/UserServices.java
@@ -0,0 +1,9 @@
+//packages
+
+
+//imports
+
+//main class - for backend functions hidden to the user
+public class UserServices {
+	
+}
