0cf418a60c324a666b320c1544dcfd4673c9a1dc
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-03-20 17:04:23 -0400

    Update GUI.java
    login button and createAccount button created and issues fix for the missing createAccount button - button size + shape modified.
    frame size changed for the login part + JTextField changed to JPasswordField

--- EmployeeClockIn-ClockOutApp/src/GUI.java
+++ EmployeeClockIn-ClockOutApp/src/GUI.java
@@ -2,11 +2,12 @@
 
 
 //imports
-import javax.swing.JButton;          //makes the window box
-import javax.swing.JFrame;          //formats the design and spacing withing the GUI window
-import javax.swing.JLabel;          //JLabel for the label such as the title for "username" and "password"
-import javax.swing.JPanel;      //for the username and password input into the textbox
-import javax.swing.JTextField;         //for button created - such as the login button
+import javax.swing.JButton;
+import javax.swing.JFrame;          //makes the window box
+import javax.swing.JLabel;          //formats the design and spacing withing the GUI window
+import javax.swing.JPanel;          //JLabel for the label such as the title for "username" and "password"
+import javax.swing.JPasswordField;      //for the username and password input into the textbox
+import javax.swing.JTextField;
 
 
 
@@ -17,15 +18,18 @@ public class GUI {
 
     public GUI() {
 
+
+
         //framing for the GUI
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();    //should be set before the use of panel within the frame object
         
         //GUI-frame visual settings
-        frame.setSize(300, 175);
+        frame.setSize(300, 270);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);     //used to display the GUI or to set it visible for the user
         frame.add(panel);
+        frame.setTitle("ClockIn/Out App: Login/Create Account");
         
         panel.setLayout(null);
 
@@ -35,9 +39,9 @@ public class GUI {
         panel.add(userLabel);
 
         //username-input textbox
-        JTextField userText = new JTextField();
-        userText.setBounds(100, 20, 165, 25);
-        panel.add(userText);
+        JTextField username = new JTextField();
+        username.setBounds(100, 20, 165, 25);
+        panel.add(username);
 
         //password label
         JLabel passwordLabel = new JLabel("Password");
@@ -45,14 +49,24 @@ public class GUI {
         panel.add(passwordLabel);
 
         //password-input textbox
-        JTextField passwordText = new JTextField();
-        passwordText.setBounds(100, 50, 165, 25);
-        panel.add(passwordText);
+        JPasswordField password = new JPasswordField();
+        password.setBounds(100, 50, 165, 25);
+        panel.add(password);
 
         //login button
-        JButton login = new JButton("Login");
-        login.setBounds( 100, 90, 75, 25);
-        panel.add(login);
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds( 100, 90, 75, 25);
+        panel.add(loginButton);
+        
+
+        //create an account button
+        JButton createAccount = new JButton("Create Account");
+        createAccount.setBounds(100, 140, 130, 25);
+        panel.add(createAccount);
+
+        
+        
+        
 
     }
 
