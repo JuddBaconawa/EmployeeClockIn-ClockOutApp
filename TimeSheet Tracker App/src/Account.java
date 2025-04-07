12d9e44c28ee8ca0784cf6d0dda92a5bace8c8d6
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-07 16:17:30 -0400

    Update Account.java
    account created with parameters + Getters are created

--- TimeSheet Tracker App/src/Account.java
+++ TimeSheet Tracker App/src/Account.java
@@ -1,17 +1,28 @@
 //packages
 
 //imports
-
-//Account class
 public class Account {
 
 	private String usernameInput;
 	private char[] passwordInput;
 	private int userId;
 
-	public Account(String usernameInput, char[] passwordInput, int userId) {
+	public Account(String usernameInput, String passwordInput, int userId) {
 		this.usernameInput = usernameInput;
-		this.passwordInput = passwordInput;
+		this.passwordInput = passwordInput.toCharArray();
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return usernameInput;
+	}
+
+	public char[] getPassword() {
+		return passwordInput;
+	}
+
+	public int getUserId() {
+		return userId;
 	}
 	
 	
