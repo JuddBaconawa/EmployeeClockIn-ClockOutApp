09a5cf07c1f32faf7dfe2ec24b16fedab0cafe1f
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-07 18:39:21 -0400

    Update UserService.java
    boolean for credential checks populated + accounts for test added

--- TimeSheet Tracker App/src/UserService.java
+++ TimeSheet Tracker App/src/UserService.java
@@ -1,10 +1,35 @@
 //packages
 
 //imports
+import java.util.ArrayList;
+import java.util.Arrays;
 
 //UserService class
 public class UserService {
 
+	//ArrayList for accounts to be populated in
+	static ArrayList <Account> accounts = new ArrayList<>();
 	
-	
+	//used to add the users credentials for logging in
+	public static void initializeUsers() {
+		accounts.add(new Account("admin", "password01", 000001));
+		accounts.add(new Account("user1", "password1", 000002));
+		accounts.add(new Account("dulce", "aswang", 0000003));
+	}
+
+	public static boolean login(String usernameInput, char[] passwordInput, ArrayList<Account> account) {
+		for (Account accounts : account) {
+
+			if (accounts.getUsername().equals(usernameInput) && Arrays.equals(accounts.getPassword(), passwordInput)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	// public static ArrayList<Account> getAccounts() {
+	// 	return UserServices.accounts;
+	// }
+
 }
