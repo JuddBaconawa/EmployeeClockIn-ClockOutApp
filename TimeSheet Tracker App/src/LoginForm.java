1cceb164df53e8e7e35c030a058e994e4c3c11ac
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-21 19:55:13 -0400

    Update LoginForm.java
    imports: color + JButton + WindowConstants
    
    LeftPanel:
    JLabel for left panel added + left title + leftPanel background set
    
    RightPanel:
    RightPanel Created with format + comments.
    right panel components added - usernameLabel, usernameInput, passwordLabel, and userPasswordInput
    
    LoginButton + Exit Button added + bounds set
    
    InnerMainPanel created + leftpane and rightpane added.
    
    frame settings - title added, hideOnClose, set size, set location relative, MainPanel added,

--- TimeSheet Tracker App/src/LoginForm.java
+++ TimeSheet Tracker App/src/LoginForm.java
@@ -4,12 +4,15 @@
 /****************** imports **************************/
 // Toolkit for GUI
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 // LoginForm
 public class LoginForm extends JFrame {
@@ -24,14 +27,25 @@ public class LoginForm extends JFrame {
 
 		//create the main panel to hold the left and right panels
 		JPanel innerMainPanel = new JPanel(new GridLayout(1, 2));
+	
 
 		/*********************** Left Panel ************************/
 		JPanel leftPanel = new JPanel(new BorderLayout());
-		JLabel lefttitle = new JLabel("Welcome Back!", JLabel.CENTER);
+		JLabel leftTitle = new JLabel("Welcome Back!", JLabel.CENTER);
+		leftPanel.setBackground(RGB(243, 344, 445));
+		
+		//future image
+		//image label
+		//leftPanel.add(imageLabel, BorderLayout.CENTER);
+
+
+		/******************** Form Panel Left **********************/
+		leftPanel.add(leftTitle, BorderLayout.CENTER);
 
 
 		/*********************** Right Form panel ********************/
 		/**************** Labels and Inputs panel *****************/
+		JPanel rightPanel = new JPanel(new GridLayout(3, 3, 10, 10));
 
 		JLabel usernameLabel = new JLabel("Username");
 		usernameLabel.setBounds(25, 50, 75, 25);
@@ -45,30 +59,48 @@ public class LoginForm extends JFrame {
 		userPasswordInput = new JPasswordField();
 		userPasswordInput.setBounds(75, 90, 75, 25);
 
-		/*********************** Form panel ********************/
+		/*********************** Form panel RIGHT ********************/
 		JPanel formPanel = new JPanel();
-		formPanel.add(usernameLabel);
-		formPanel.add(usernameInput);
-		formPanel.add(passwordLabel);
-		formPanel.add(userPasswordInput);
+		rightPanel.add(usernameLabel);
+		rightPanel.add(usernameInput);
+		rightPanel.add(passwordLabel);
+		rightPanel.add(userPasswordInput);
+
+		
 
 
 		/*********************** Buttons panel ********************/
+		JButton loginButton = new JButton("Log In");
+		loginButton.setBounds(75, 25, 50, 25);
+
+		JButton windowExit = new JButton("Exit");
+		windowExit.setBounds(25, 25, 50, 25);
 
 
 		/*********************** Button-Form panel ********************/
+		rightPanel.add(loginButton);
+		rightPanel.add(windowExit);
 
+		/*********************** Inner Main panel ********************/
+		innerMainPanel.add(leftPanel);
+		innerMainPanel.add(rightPanel);
+		
 
 		/*********************** Frame Settings ********************/
-		setTitle("Login");
-		setDefaultCloseOperation(HIDE_ON_CLOSE);
-		setSize(400, 300);
-		add(formPanel);
+		setTitle("Login to Clock In");
+		setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+		setSize(600, 400);
+		setLocationRelativeTo(null);
+		add(innerMainPanel);
 		setVisible(true);
 
 		
 
 	}
+
+    private Color RGB(int i, int i0, int i1) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 
 }
