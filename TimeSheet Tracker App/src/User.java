2d518800ffc25445b27b95bf4b3a266ab612a83b
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-04-22 16:20:29 -0400

    Create User.java
    User class created for users that are logging in or are clocking in. User class attributes added: userId, username, email, phone, address, and password

---
+++ TimeSheet Tracker App/src/User.java
@@ -0,0 +1,16 @@
+// PACKAGES
+
+
+// IMPORTS
+
+// USER - class
+public class User {
+	
+	public int userId;
+	public String username;
+	public String email;
+	public String phone;
+	public String address;
+	public String password;
+
+}
