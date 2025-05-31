0505794288c88ab36727a7aeced4c855bcee7866
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-05-31 18:20:01 -0400

    Update DisplayCard.java
    DisplayCard which is a base class is created and populated - setbounds to match the DisplayPanel on the TimeSheet class

--- TimeSheet Tracker App/src/DisplayCard.java
+++ TimeSheet Tracker App/src/DisplayCard.java
@@ -1,3 +1,15 @@
-public class DisplayCard {
+// IMPORTS
+
+import javax.swing.JPanel;
+import java.awt.Color;
+
+public class DisplayCard extends JPanel {
+    public DisplayCard(String name) {
+        setName(name);
+        setLayout(null);
+        setBackground(new Color(240, 235, 216));
+        setBounds(200, 120, 1000, 680);         //location and size of a card
+        
   
-}
+    }
+  }
