6f4c3f1bd484359f95b80777ac32c9ca472974ff
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-06-04 22:20:38 -0400

    Update Home.java
    Home example created

--- TimeSheet Tracker App/src/Home.java
+++ TimeSheet Tracker App/src/Home.java
@@ -20,5 +20,11 @@ public class Home extends DisplayCard {
       //Home instance created for testing
       Home home = new Home();
 
+      javax.swing.JFrame frame = new javax.swing.JFrame("Home Example");
+      setDefaultOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+      frame.add(home);
+      frame.setSize(1000, 680);
+      frame.setVisible(true);
+
     }
 }  
