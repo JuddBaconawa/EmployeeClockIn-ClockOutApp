b57e35268c8fd23f7e008a366ade4355b68cbc87
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-06-04 22:34:19 -0400

    Update Profile.java
    Profile class extended by the DisplayCard class and populated

--- TimeSheet Tracker App/src/Profile.java
+++ TimeSheet Tracker App/src/Profile.java
@@ -1,3 +1,11 @@
-public class Profile {
+// IMPORTS
+import java.awt.Color;
+
+
+// Profile Class
+public class Profile extends DisplayCard {
+    public Profile() {
+        super("Profile");
+        setBackground(new Color(62, 92, 118));
   
 }
