61ffa066ae1cc7bdc20765c3321a3dc45dd35ff9
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-06-04 22:31:30 -0400

    Update Timelog.java
    Timelog class populated and defined

--- TimeSheet Tracker App/src/Timelog.java
+++ TimeSheet Tracker App/src/Timelog.java
@@ -1,3 +1,12 @@
-public class Timelog {
+// IMPORTS
+import java.awt.Color;
+
+// TimeLog class
+public class Timelog extends DisplayCard {
+    public Timelog() {
+      super("Timelog");
+      setBackground(new Color(0, 40, 0));
+      // additional initialization if needed
+      } 
   
 }
