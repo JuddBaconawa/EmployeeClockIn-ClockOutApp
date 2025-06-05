40ba13ed60f52941bba2d92d037afe0fdc0e76a1
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-06-05 11:17:39 -0400

    Update Profile.java
    Override portion of the Profile class for giving a title to the card + text size and color to match the background

--- TimeSheet Tracker App/src/Profile.java
+++ TimeSheet Tracker App/src/Profile.java
@@ -9,5 +9,12 @@ public class Profile extends DisplayCard {
         setBackground(new Color(62, 92, 118));
   
     }
+
+    @Override
+    protected void paintComponent(java.awt.Graphics g) {
+        super.paintComponent(g);
+        g.setFont(new java.awt.Font("arial", java.awt.Font.BOLD, 32));
+        g.setColor(java.awt.Color.LIGHT_GRAY);
+        g.drawString("Profile", 30, 60);
+    }
     
-}
