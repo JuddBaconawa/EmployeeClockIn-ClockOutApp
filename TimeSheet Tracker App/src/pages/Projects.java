72d78fd46a095a8290ce3f8d430f71b8fcbd07cc
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-01 22:52:13 -0400

    Projects.java edited
    Title added + bounds set and size

--- TimeSheet Tracker App/src/pages/Projects.java
+++ TimeSheet Tracker App/src/pages/Projects.java
@@ -1,5 +1,19 @@
+// PACKAGES
 package pages;
 
+// IMPORTS
+import  components.DisplayCard;
+
 public class Projects extends DisplayCard {
+
+  public Projects() {
+
+    // Card Title
+    super("Projects");
+    setLayout(null);
+    setBounds(200, 120, 1200, 680);
+    
+
+  }
   
 }
