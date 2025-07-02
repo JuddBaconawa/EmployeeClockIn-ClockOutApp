11d79099cad060e3a0759a472b1464b0ddcdfd11
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-02 17:20:28 -0400

    Projects.java edited
    background set + new panel created
    
    color and JPanel tools imported

--- TimeSheet Tracker App/src/pages/Projects.java
+++ TimeSheet Tracker App/src/pages/Projects.java
@@ -3,6 +3,8 @@ package pages;
 
 // IMPORTS
 import  components.DisplayCard;
+import java.awt.Color;
+import javax.swing.JPanel;
 
 public class Projects extends DisplayCard {
 
@@ -12,6 +14,11 @@ public class Projects extends DisplayCard {
     super("Projects");
     setLayout(null);
     setBounds(200, 120, 1200, 680);
+    setBackground(new Color(255, 255, 255));
+
+
+    JPanel projectsPanel = new JPanel();
+    
     
 
   }
