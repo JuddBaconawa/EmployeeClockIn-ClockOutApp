6d402fff0010e7927aceb205c63d3ee701c6f739
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-03 15:36:37 -0400

    Projects.java edited
    Imports added for JPanel, JLabel, Font, and FlowLayout
    
    layout set to null
    
    titlePanel, titleLabel added with configurations such as text, size, or placements
    
    ProjectsPanel created + bounds set to show

--- TimeSheet Tracker App/src/pages/Projects.java
+++ TimeSheet Tracker App/src/pages/Projects.java
@@ -3,8 +3,17 @@ package pages;
 
 // IMPORTS
 import  components.DisplayCard;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+
 
 public class Projects extends DisplayCard {
 
@@ -12,15 +21,31 @@ public class Projects extends DisplayCard {
 
     // Card Title
     super("Projects");
-    setLayout(null);
+    // setLayout(null);
     setBounds(200, 120, 1200, 680);
-    setBackground(new Color(255, 255, 255));
+    setBackground(new Color(62, 92, 118));
 
+    // Displaycard Title that displays
+    JPanel titlePanel = new JPanel();
+    titlePanel.setBackground(new Color(62, 92, 118));
+    titlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    JLabel titleLabel = new JLabel("Projects");
+    titleLabel.setFont(new Font("Arial", Font.BOLD, 32));
+    titleLabel.setForeground(Color.DARK_GRAY);
+    titlePanel.add(titleLabel);
+    
+    // this.setLayout(new BorderLayout());
+    this.add(titlePanel);
 
-    // Projects panel
+    // Projects panel bar
     JPanel projectsPanel = new JPanel();
-    projectsPanel.setBounds(5, 10, 990, 20);
-    projectsPanel.setBorder(new Border());
+    projectsPanel.setBounds(25, 70, 1150, 20);
+    // projectsPanel.setBorder(BorderFactory.createEtchedBorder(5, Color.GRAY, Color.LIGHT_GRAY));
+
+    
+
+    // =================== Add to Projects =======================
+    add(projectsPanel);
 
     
 
