7dd29423855daad8deb36ab1b7429c2593650189
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-07 16:47:31 -0400

    Dashboard.java edited
    imports added
    
    paintComponent deleted
    
    JPanel "titlePanel" created

--- TimeSheet Tracker App/src/pages/Dashboard.java
+++ TimeSheet Tracker App/src/pages/Dashboard.java
@@ -1,8 +1,13 @@
 package pages;
+
 // IMPORTS
+import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.JPanel;
+
 import components.DisplayCard;
+
 
 // Dashboard class
 public class Dashboard extends DisplayCard {
@@ -10,22 +15,12 @@ public class Dashboard extends DisplayCard {
     public Dashboard() {
         // Card Title
         super("Dashboard");
-        
-        setBackground(new Color(240, 235, 216));
 
-        
-    }
-  
-    @Override
-    protected void paintComponent(java.awt.Graphics g) {
-        super.paintComponent(g);
-        java.awt.Graphics2D g2d = (java.awt.Graphics2D) g;
-        g2d.setRenderingHint(java.awt.RenderingHints.KEY_ANTIALIASING,
-                             java.awt.RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 32));
-        g2d.setColor(java.awt.Color.DARK_GRAY);
-        g2d.drawString("Dashboard", 30, 60);
-    }
+        setBackground(new Color(240, 235, 216));
+        setLayout(new BorderLayout());
+
+        JPanel titlePanel = new JPanel();
+    }  
 
     // public static void main(String[] args) {
     //     // Create an instance of Dashboard to test
