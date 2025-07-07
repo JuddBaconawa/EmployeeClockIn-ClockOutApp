e60cea033469af325a7b21cdad9334897d06bdb7
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-07 16:46:28 -0400

    DisplayCard.java edited
    fixed issues with Displaycard such as the layout and preferred size

--- TimeSheet Tracker App/src/components/DisplayCard.java
+++ TimeSheet Tracker App/src/components/DisplayCard.java
@@ -2,15 +2,25 @@ package components;
 // IMPORTS
 
 import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 
 public class DisplayCard extends JPanel {
     
     public DisplayCard(String name) {
         setName(name);
-        // setLayout(null);
+        
+        // Styling
         setBackground(new Color(240, 235, 216));
         setBounds(200, 120, 1200, 680);         //location and size of a card
+
+        // Layout
+        setLayout(new BorderLayout());
+
+        // Set Preferred size hint
+        setPreferredSize(new Dimension(1200, 680));
         
   
     }
