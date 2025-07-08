7eef65ce204645ab29563adfd2bac6b32091b407
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-08 18:10:16 -0400

    Dashboard.java edited
    imports added
    
    titlePanel added + label and indicator panel
    
    contentpanel created + added
    
    updateStatus method created

--- TimeSheet Tracker App/src/displayCards/Dashboard.java
+++ TimeSheet Tracker App/src/displayCards/Dashboard.java
@@ -3,12 +3,16 @@ package displayCards;
 // IMPORTS
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import components.DisplayCard;
+import components.dashboard.ClockPanel;
+import components.dashboard.TimeStatsPanel;
+import components.dashboard.ProfilePanel;
 
 
 // Dashboard class
@@ -24,12 +28,30 @@ public class Dashboard extends DisplayCard {
         // === Title Panel ===
         JPanel titlePanel = new JPanel();
         titlePanel.setBackground(new Color(62, 92, 118));
-        titlePanel.setAlignmentX(JPanel.LEFT_ALIGNMENT);
+
         JLabel titleLabel = new JLabel("Dashboard");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 32));
         titleLabel.setForeground(new Color(255, 255, 255));
 
-        // === 
+        // Status Indicator (small Colered Circle)
+        
+
+        titlePanel.add(titleLabel);
+        titlePanel.add(indicatorPanel);
+
+        // === Content Panel === 
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 20));
+        contentPanel.setOpaque(false);
+
+        contentPanel.add(new TimeStatsPanel());
+        contentPanel.add(new ClockPanel());
+
+
+        // Add panels to layout
+        add(titlePanel, BorderLayout.NORTH);
+        add(contentPanel, BorderLayout.CENTER);
+
         
 
         
@@ -37,7 +59,22 @@ public class Dashboard extends DisplayCard {
 
         titlePanel.add(titleLabel);
         add(titlePanel);
-    }  
+    }
+    
+    public void updateStatus(String status) {
+    statusIndicator.setStatus(status);
+    switch (status) {
+        case "in":
+            statusText.setText("Logged In");
+            break;
+        case "break":
+            statusText.setText("On Break");
+            break;
+        case "out":
+        default:
+            statusText.setText("Logged Out");
+        }
+    }
 
     // public static void main(String[] args) {
     //     // Create an instance of Dashboard to test
