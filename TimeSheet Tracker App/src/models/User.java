9de1f343f00cc7bb5af06076e990379c2ab18c1f
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-09 16:35:39 -0400

    User.java edited
    import for image added
    
    other declaration added
    
    users set to new
    
    new method added: getCurrentUser + setCurrentUser

--- TimeSheet Tracker App/src/models/User.java
+++ TimeSheet Tracker App/src/models/User.java
@@ -3,22 +3,38 @@ package models;
 
 
 // IMPORTS
+import java.awt.Image;
 
 // USER - class
 public class User {
 	
-	// User class' Attributes
-	public int userId;
-	public String username;
-	public String role;
-	public String email;
-	public String phone;
-	public String address;
-	public String password;
+		// User class' Attributes
+		public java.awt.Image profilePicture;
+		public String firstName;
+		public String username;
+		public String department;
+		public String role;
+		public String email;
+		public String phone;
+		public String address;
+		public String password;
 
-	    public User() {
-        this.username = "SampleUser";
-        this.role = "Employee";
-    }
+		private static User currentUser = new User();
+
+		public User() {
+			this.username = "SampleUser";
+			this.role = "Employee";
+			this.department = "General";
+			
+		}
+
+		public static User getCurrentUser() {
+			return currentUser;
+		}
+
+		// Optionally, add a setter if you want to change the current user:
+		public static void setCurrentUser(User user) {
+			currentUser = user;
+		}
 
 }
