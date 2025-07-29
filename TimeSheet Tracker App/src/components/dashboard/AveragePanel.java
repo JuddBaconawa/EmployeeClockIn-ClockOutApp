7396b5617afb9ae3171d58271773e389baaa1bb2
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-07-29 15:29:13 -0400

    Update AveragePanel.java
    AveragePanel.java class populated with the code from the former StreakPanel.java

--- TimeSheet Tracker App/src/components/dashboard/AveragePanel.java
+++ TimeSheet Tracker App/src/components/dashboard/AveragePanel.java
@@ -1,9 +1,22 @@
+// PACKAGE DECLARATION
 package components.dashboard;
 
+// IMPORTS 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+
+// Swing imports
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
-public class AveragePanel {
+public class AveragePanel extends JPanel{
 
     // donut graph metrics
     private String[] metrics = {
