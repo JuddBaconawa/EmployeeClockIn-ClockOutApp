3bab3eb69de6d8097ddf6f1d6bf1faa4e0b8ff6d
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-11-07 22:29:34 -0500

    Update DBConnection.java
    DBConnection.java class created and set.

--- TimeSheet Tracker App/src/util/DBConnection.java
+++ TimeSheet Tracker App/src/util/DBConnection.java
@@ -1,5 +1,24 @@
+// PACKAGE - Folder name
 package util;
 
+// IMPORTS
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+  // Database credentions for log in
+  private static final String URL = "jdbc:mysql://localhost:3306/timesheetappdatabase";
+  private static final String USER = "root";
+  private static final String PASSWORD = "";
+
+  // Set private to prevent instantiation
+  private DBConnection() {}
+
+  // Returns a new Connection each time
+  public static Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(URL, USER, PASSWORD);
+  }
   
 }
