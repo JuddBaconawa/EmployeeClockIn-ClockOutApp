39d0071383a48d494a9d5672c7e9dfaa803b438c
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-11-11 23:25:06 -0500

    Update LoadProjectsFromDB.java
    new import added: sql.
    
    connection part corrected to be reusable

--- TimeSheet Tracker App/src/util/LoadProjectsFromDB.java
+++ TimeSheet Tracker App/src/util/LoadProjectsFromDB.java
@@ -1,13 +1,47 @@
 package util;
 
+import displayCards.Projects;
+import displayCards.Projects.Project;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoadProjectsFromDB {
 
-  List<Project> projectList = new ArrayList<>();
+  private Connection conn;
+
+  public LoadProjectsFromDB(Connection conn) {
+      this.conn = conn;
+  }
+
+  public List<Project> loadProjects() {
+      List<Project> projectList = new ArrayList<>();
+
   try{
       String sql = "SELECT name, hours_logged, max_hours FROM projects";
       PreparedStatement stmt = conn.prepareStatement(sql);
       ResultSet rs = stmt.executeQuery();
+
+
+      while (rs.next()) {
+          projectList.add(new Project(
+              rs.getString("name"),
+              rs.getInt("hours_logged"),
+              rs.getInt("max_hours"),
+              new ArrayList<>()
+          ));
+      }
+          rs.close();
+          stmt.close();
+      } catch (SQLException e) {
+          e.printStackTrace();
+      }
+
+      return projectList;
       
   }
-  
+
 }
