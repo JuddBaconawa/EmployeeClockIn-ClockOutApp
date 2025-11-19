364f2c48c5b1bc1991e7fecc2f717d4d0316e51d
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-11-18 19:30:10 -0500

    Update TimelogEntry.java
    imports added + fields for the one time entry which are set to private so that it cannon be edited
    
    TimelogEntry object constructor

--- TimeSheet Tracker App/src/models/TimelogEntry.java
+++ TimeSheet Tracker App/src/models/TimelogEntry.java
@@ -1,7 +1,26 @@
+// Packages
 package models;
 
+// IMPORTS
+import java.sql.Timestamp;
+import java.sql.Date;
 
-
+// Class to create an object for each timelog entry
 public class TimelogEntry {
+
+    private String projectName;
+    private Timestamp clockIn;
+    private Timestamp clockOut;
+    private double totalHours;
+    private Date workDate;
+
+    // Constructor for the TimelogEntry class object
+    public TimelogEntry(String projectName, Timestamp clockIn, Timestamp clockOut, double totalHours, Date workDate) {
+        this.projectName = projectName;
+        this.clockIn = clockIn;
+        this.clockOut = clockOut;
+        this.totalHours = totalHours;
+        this.workDate = workDate;
+    } 
   
 }
