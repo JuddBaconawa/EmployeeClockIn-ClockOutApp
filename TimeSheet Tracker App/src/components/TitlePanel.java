95aebe481028df6403a1ec622a4e6332d381ad82
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-11-21 22:05:36 -0500

    Update TitlePanel.java
    title panel fixed, unused code removed

--- TimeSheet Tracker App/src/components/TitlePanel.java
+++ TimeSheet Tracker App/src/components/TitlePanel.java
@@ -16,27 +16,31 @@ import javax.swing.JPanel;
 public class TitlePanel extends JPanel{
 
     // Declaration
-    private static StatusIndicator statusIndicator;
-    private JPanel titlePanel;
     private JLabel titleLabel;
 
     // Title Panel Constructor
     public TitlePanel(String title, StatusIndicator statusIndicator) {
-        this.statusIndicator = statusIndicator;
+        
 
         // Title Panel
-        JPanel titlePanel = new JPanel(new BorderLayout());
+        setLayout(new BorderLayout());
         setBackground(new Color(66, 217, 200));
         setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
 
-        JLabel titleLabel = new JLabel(title);
+        // set title label + format and color
+        titleLabel = new JLabel(title);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 32));
         titleLabel.setForeground(new Color(66, 217, 200));
 
+        // Add the title label to the far left
         add(titleLabel, BorderLayout.WEST);
-        add(statusIndicator, BorderLayout.EAST);
+        if (statusIndicator != null) {
+            // adds the the status indicator to the far right
+            add(statusIndicator, BorderLayout.EAST);
+        }
     }
 
+    // Method to update the title
     public void setTitle(String title) {
         titleLabel.setText(title);
     }
