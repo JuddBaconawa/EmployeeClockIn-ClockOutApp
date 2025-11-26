f1bc067c40fa89d28b4e18420cd92285faf6a739
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-11-26 18:06:13 -0500

    Update TitlePanel.java
    Border for the titlepanel/label set to show the Displaycard title
    
    text changed: set size to 17 and made BOLD
    
    statusPanel created - statusIndicator and StatusText added to the statusPanel
    
    statusPanel set to East - to display userStatus

--- TimeSheet Tracker App/src/components/TitlePanel.java
+++ TimeSheet Tracker App/src/components/TitlePanel.java
@@ -29,20 +29,27 @@ public class TitlePanel extends JPanel{
         // Title Panel
         setLayout(new BorderLayout());
         setBackground(new Color(66, 217, 200));
-        setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        setBorder(BorderFactory.createEmptyBorder(10, 20, 0, 20));
 
         // set title label + format and color (left side)
         titleLabel = new JLabel(title); 
         titleLabel.setFont(new Font("Arial", Font.BOLD, 32));
         titleLabel.setForeground(new Color(255, 255, 255));
-        setPreferredSize(new Dimension(0, 60)); // ensure height
+        setPreferredSize(new Dimension(0, 65)); // ensure height
         add(titleLabel, BorderLayout.WEST);
 
         // Add the title label to the far right
         statusIndicator = new StatusIndicator();
         statusText = new JLabel("Status");
-        statusText.setFont(new Font("Arial", Font.PLAIN, 16));
+        statusText.setFont(new Font("Arial", Font.BOLD, 17));
         statusText.setForeground(Color.WHITE);
+
+        JPanel statusPanel = new JPanel();
+        statusPanel.setOpaque(false);
+        statusPanel.add(statusIndicator);
+        statusPanel.add(statusText);
+
+        add(statusPanel, BorderLayout.EAST);
 
         
     }
