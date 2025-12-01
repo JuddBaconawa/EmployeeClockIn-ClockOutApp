0e58b0471b813b563d0e9c7d0e14b9e6a10208c8
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-12-01 12:07:52 -0500

    Create AddProjectDialog.java
    AddProjectDialog class created
    
    declarations added
    
    import added for JDialog
    
    AddprojectDialog constructor started

---
+++ TimeSheet Tracker App/src/displayCards/AddProjectDialog.java
@@ -0,0 +1,19 @@
+package displayCards;
+
+import javax.swing.JDialog;
+
+public class AddProjectDialog extends JDialog{
+
+    private JTextField projectNameField;
+    private JTextArea projectDescriptionArea;
+    private JTextField hoursField;
+    private JTextField maxHoursField;
+    private JTextField deadlineField;
+
+    private boolean confirmed = false;
+
+    public AddProjectDialog(JFrame parent) {
+        
+    }
+  
+}
