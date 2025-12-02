7807e8ec8fb58b889b3bb297022370516f759649
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-12-01 21:52:55 -0500

    Update AddProjectDialog.java
    important imports added
    
    Project dialog contents added such as addrows for user input to fill the Project name, max hours, deadline, and project description
    
    Buttons for confirming and cancelling added
    
    buttons added to the formPanel - set CENTER
    
    actionListener added(incomplete)

--- TimeSheet Tracker App/src/displayCards/AddProjectDialog.java
+++ TimeSheet Tracker App/src/displayCards/AddProjectDialog.java
@@ -1,11 +1,13 @@
 package displayCards;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JDialog;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -40,7 +42,35 @@ public class AddProjectDialog extends JDialog{
         JPanel formPanel = new JPanel(new GridBagLayout());
         formPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        
+         GridBagConstraints gbc = new GridBagConstraints();
+         gbc.insets = new Insets(5, 5, 5, 5);
+         gbc.fill = GridBagConstraints.HORIZONTAL;
+
+         int row = 0;
+
+         // Project name
+         addRow(formPanel, gbc, row++, "Project Name:", projectNameField);
+
+         // Max Hours
+         addRow(formPanel, gbc, row++, "Max Hours: ", maxHoursField);
+
+         // DeadLine
+         addRow(formPanel, gbc, row++, "Deadline: ", deadlineField);
+
+         // Project Description
+         addRow(formPanel, gbc, row++, "Project Description: ", projectDescriptionArea);
+
+         // BUTTONS
+         JPanel buttonPanel = new JPanel();
+         JButton confirmButton = new JButton("Confirm");
+         JButton cancelButton = new JButton("Cancel");
+         buttonPanel.add(confirmButton);
+         buttonPanel.add(cancelButton);
+
+         add(formPanel, BorderLayout.CENTER);
+
+         confirmButton.addActionListener(this::onConfirm);
+         cancelButton.addActionListener(e -> dispose());
 
     }
   
