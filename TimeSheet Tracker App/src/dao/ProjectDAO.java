a46f3dc002f3966485f8cdaf2364ce266b341ea4
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-12-15 23:35:34 -0500

    Create ProjectDAO.java
    ProjectDAO class created and filled

---
+++ TimeSheet Tracker App/src/dao/ProjectDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+// IMPORTS
+import models.User;
+import displayCards.Projects.Project;
+import util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProjectDAO {
+
+    // constructor for the connection
+    private Connection conn;
+
+    // initialize connection
+    public ProjectDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    //Create a new project in the database
+    public void createProject(Project project) {
+        // SQL Insert Statement
+        String sql = "INSERT INTO projects (user_id, name, max_hours, deadline) VALUES (?, ?, ?, ?)";
+        
+        try (PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, userId);
+            ps.setString(2, project.name);
+            ps.setInt(3, project.maxHours);
+            ps.setString(4, project.deadline);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+  
+}
