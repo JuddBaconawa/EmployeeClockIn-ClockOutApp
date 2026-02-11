76b80a48c7c63faa55e6a8bf8292661661edeaf1
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2026-02-11 01:15:52 -0500

    Updated TimeSheet.class
    unused code removed

--- TimeSheet Tracker App/src/core/App.java
+++ TimeSheet Tracker App/src/core/App.java
@@ -20,17 +20,7 @@ public class App {
 			TimeSheet timesheet = new TimeSheet();
 			timesheet.initialize(user);
 				
-			// LoginForm loginForm = new LoginForm();		
-			// loginForm.initialize();
 
-			// TimeSheet timeSheet = new TimeSheet(new User());
-			// timeSheet.initialize(new User());
-
-
-			// Create a User object to pass to initialize
-			// User user = new User(); // You may need to set properties or use a constructor
-			// TimeSheet timeSheet = new TimeSheet(user);
-			// timeSheet.initialize(user);
 
 		});
 
