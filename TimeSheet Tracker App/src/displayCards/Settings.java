ca4ec5ee91acb4aaad94f5cd0d8c31f11be80d97
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2026-02-25 01:48:21 -0500

    Updated Settings.java
    border imported, titlepanel added
    
    content panel layout changed + added to the settings display card
    
    footerpanel created and added SOUTH
    
    JPanel - passwordPanel created and added to the content panel

--- TimeSheet Tracker App/src/displayCards/Settings.java
+++ TimeSheet Tracker App/src/displayCards/Settings.java
@@ -8,6 +8,7 @@ import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 import components.DisplayCard;
 import components.dashboard.StatusIndicator;
@@ -36,35 +37,44 @@ public class Settings extends DisplayCard {
       // TITLE Panel
       TitlePanel titlePanel = new TitlePanel("Settings", new StatusIndicator());
       titlePanel.setBackgroundColor(new Color(89, 92, 118));
+      add(titlePanel, BorderLayout.NORTH);
 
       
 
       // Content Panel
       JPanel contentPanel = new JPanel();
-      contentPanel.setLayout(new GridLayout(2, 2, 10, 50));
+      contentPanel.setLayout(new GridLayout(1, 2, 10, 50));
       contentPanel.setOpaque(true);
       contentPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 20, 20));
+      add(contentPanel, BorderLayout.CENTER);
 
       JPanel footerPanel = new JPanel();
       footerPanel.setBackground(new Color(35, 177, 85));
-      footerPanel.setOpaque(false);
-
+      footerPanel.setOpaque(true);
+      footerPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+      add(footerPanel, BorderLayout.SOUTH);
 
       
-      
+      // Add components to the Home JPanel
 
 
 
       // additional initialization if needed
       JLabel usernameLabel = new JLabel ("Welcome, " + "User!" + "This is the settings"); // Replace "User" with actual username
-      usernameLabel.setBounds(25, 150, 200, 30);
       this.add(usernameLabel);
 
+      // settings panel
+      JPanel passwordPanel = new JPanel();
+      passwordPanel.setBackground(new Color(5, 199, 242));
+      passwordPanel.setOpaque(true);
+      passwordPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+      passwordPanel.add(new JLabel("Change Password:"));
+      contentPanel.add(passwordPanel);
 
-      // Add components to the Home JPanel
-      add(titlePanel, BorderLayout.NORTH);
-      add(contentPanel, BorderLayout.CENTER);
-      add(footerPanel, BorderLayout.SOUTH);
+      
+
+
+      
 
     }
 
