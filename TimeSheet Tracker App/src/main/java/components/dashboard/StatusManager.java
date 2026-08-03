5113cb61afc33aea1f4ac574dc7e2f0588882618
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2026-08-03 00:47:14 -0400

    Create StatusManager.java
    StatusManager method started - imported added + list started for indicators

---
+++ TimeSheet Tracker App/src/main/java/components/dashboard/StatusManager.java
@@ -0,0 +1,12 @@
+package components.dashboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatusManager {
+
+  private List<StatusIndicator> indicators = new ArrayList<>();
+
+  
+
+}
