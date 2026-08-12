68a2819d2fcdcfc3b9edcd5d18aecdf941b55bfb
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2026-08-12 19:32:45 -0400

    Update AdminDashboard.java
    imports added
    
    private declaration
    
    AdminDashboard constrcutor created + parameter list added
    
    statusManager declared this.  = use current

--- TimeSheet Tracker App/src/main/java/displayCards/AdminDashboard.java
+++ TimeSheet Tracker App/src/main/java/displayCards/AdminDashboard.java
@@ -1,12 +1,29 @@
 package displayCards;
 
 // IMPORTS
+import components.dashboard.StatusIndicator;
+import components.dashboard.StatusManager;
+import components.TitlePanel;
+
+import javax.swing.JLabel;
+
 
 
 // Admin Dashboard class
 public class AdminDashboard {
 
+    private StatusIndicator statusIndicator;
+    private JLabel statusText;
+    private TitlePanel titlePanel;
+    private StatusManager statusManager;
+    
+    public AdminDashboard(StatusManager statusManager) {
 
+        // card title
+        super("Admin Dashboard");
 
-  
+        // statusManager declared to current statusManager
+        this.statusManager = statusManager;
+
+    }
 }
